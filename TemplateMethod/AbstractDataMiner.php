eec986fe8c7c111560a5408d8eb1a8c017017716
Logifire <Logifire@users.noreply.github.com>
2021-09-07 21:14:15 +0200

    Moved the doc-block of the template method.
    Doc-block was placed on the wrong method.

--- TemplateMethod/AbstractDataMiner.php
+++ TemplateMethod/AbstractDataMiner.php
@@ -4,7 +4,10 @@ namespace Logifire\ProgrammingPatterns\TemplateMethod;
 
 abstract class AbstractDataMiner
 {
-
+    /**
+     * Template method: lets subclasses override specific steps of the 
+     * algorithm without changing its structure.
+     */
     public function mine(string $path): string
     {
         // Perform operations common for miners...
@@ -21,9 +24,5 @@ abstract class AbstractDataMiner
         return "Data based on {$path}";
     }
 
-    /**
-     * Template method: Abstract method to perform operations that can't 
-     * be generalized in the parent class.
-     */
     abstract protected function analyzeData(string $data): string;
 }
